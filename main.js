13b7ea51c28c362ab9e1b7970cd13166e514c726
KAmron <koraboevkamron@gmail.com>
2023-03-05 01:29:40 +0500

    save1

---
+++ main.js
@@ -0,0 +1,15 @@
+var yourName = prompt('Введите своё имя');
+console.log('Я ' + yourName);
+var age = prompt('Введите свой возраст');
+console.log('Мне ' + age +' лет');
+var Exr1 = prompt('12 + 18 = ?');
+console.log('Пример 1: 12 + 18 = ' + (12 + 18) + ' Ваш ответ ' + Exr1);
+var Exr2 = prompt('100 - 1 = ?');
+console.log('Пример 2: 100 - 1 = ' + (100 - 1) + ' Ваш ответ ' + Exr2);
+var Exr3 = prompt('12 * 3 = ?');
+console.log('Пример 3: 12 * 3 = ' + (12 * 3) + ' Ваш ответ ' + Exr3);
+var Exr4 = prompt('225 / 5 = ?');
+console.log('Пример 4: 225 / 5 = ' + (225 / 5) + ' Ваш ответ ' + Exr4);
+var Exr5 = prompt('12 % 5 = ?');
+console.log('Пример 5: 12 % 5 = ' + (12 % 5) + ' Ваш ответ ' + Exr5);
+alert('Откройте консоль для просмотра')
